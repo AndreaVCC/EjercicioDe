ee7be609e83166da6ac2047283cb321cf12fb8e1
Alvaro Alvarez Olivares <alvarez.ao@outlook.com>
2020-02-12 13:16:53 -0300

    se agrega Static a los metodos

--- Facultad/src/ejercicioD/CentroDocente.java
+++ Facultad/src/ejercicioD/CentroDocente.java
@@ -6,16 +6,16 @@ import java.util.ArrayList;
 
 public class CentroDocente {
    //ArrayList < tipo de dato> Nombre = Instancia tipo ArrayList
-	ArrayList <Persona> listaPersona = new ArrayList <>(); // Hacer una lista de todas las personas
-	ArrayList <Integer> listaDespachos = new ArrayList <>();// Asignar numero de despacho ( Empleados ) 
-	ArrayList <Integer> listaseccion = new ArrayList <>();// Asignar una seccion ( P.servicio)
-	ArrayList <String> listaCurso = new ArrayList <>();// Asignar un curso (Estudiantes)
-	ArrayList <String> listaDepartamento = new ArrayList <>();// Asignar Departamento ( Profesores ) 
+	static ArrayList <Persona> listaPersona = new ArrayList <>(); // Hacer una lista de todas las personas
+	static ArrayList <Integer> listaDespachos = new ArrayList <>();// Asignar numero de despacho ( Empleados ) 
+	static ArrayList <Integer> listaseccion = new ArrayList <>();// Asignar una seccion ( P.servicio)
+	static ArrayList <String> listaCurso = new ArrayList <>();// Asignar un curso (Estudiantes)
+	static ArrayList <String> listaDepartamento = new ArrayList <>();// Asignar Departamento ( Profesores ) 
 	
 	
 	//Metodo mostrar persona ingresada en lista
 	
-	public void muestraPersona(){
+	public static void muestraPersona(){
 		for(Persona personita: listaPersona) {
 			System.out.println(personita);
 		}
