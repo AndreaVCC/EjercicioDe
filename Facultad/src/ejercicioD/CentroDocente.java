5f1762b46164e23206541561624d901253280d56
carlos <carlos@192.168.0.174>
2020-02-12 13:02:13 -0300

    ArrayList

--- Facultad/src/ejercicioD/CentroDocente.java
+++ Facultad/src/ejercicioD/CentroDocente.java
@@ -7,9 +7,10 @@ import java.util.ArrayList;
 public class CentroDocente {
    //ArrayList < tipo de dato> Nombre = Instancia tipo ArrayList
 	ArrayList <Persona> ListaPersona = new ArrayList <>();
-	
-	
-	
+	ArrayList <Integer> ListaDespachos = new ArrayList <>();
+	ArrayList <Integer> Listaseccion = new ArrayList <>();
+	ArrayList <String> ListaCurso = new ArrayList <>();
+	ArrayList <String> ListaDepartaento = new ArrayList <>();
 	
 	
 
