fcba00d70728504a1a2fae028797a8a2438b6c33
Alvaro Alvarez Olivares <alvarez.ao@outlook.com>
2020-02-13 12:17:32 -0300

    # WARNING: head commit changed in the meantime
    metodo CambiarSeccion

--- Facultad/src/ejercicioD/PersonalServicio.java
+++ Facultad/src/ejercicioD/PersonalServicio.java
@@ -1,4 +1,5 @@
 package ejercicioD;
+import java.util.ArrayList;
 
 public class PersonalServicio extends Persona {
 
@@ -24,6 +25,24 @@ public class PersonalServicio extends Persona {
 
 	public String getSeccion() {
 		return seccion;
+	}
+	
+	public void cambiarSeccion(ArrayList<String> listaseccion, Integer cambio ) {
+		boolean d = false;
+		
+		for(int i = 0; i<listaseccion.size(); i++) {
+			if(listaseccion.get(i).equals(cambio)) {
+				//Cambiamos valor Integer a String
+				this.seccion = Integer.toString(cambio);
+				d = true;
+			}
+		}
+		
+		if (d) {
+			System.out.println("Cambio Exitoso");
+		} else {
+			System.out.println("No se pudo realizar el cambio");
+		}
 	}
 
 	public String toString() {
