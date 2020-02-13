c8ce845fc297c01f45f46fe4a3888861ceb68169
danny-huasco <60190324+danny-huasco@users.noreply.github.com>
2020-02-13 11:11:00 -0300

    Update Estudiante.java

--- Facultad/src/ejercicioD/Estudiante.java
+++ Facultad/src/ejercicioD/Estudiante.java
@@ -1,40 +1,75 @@
 package ejercicioD;
 
+import java.util.ArrayList;
+
 public class Estudiante extends Persona{
     
 	//ATRIBUTO  
-	private String curso;
+	private ArrayList<String> cursos = new ArrayList<>();
 
 	//CONSTRUCTOR 
-	
 	public Estudiante() {
 		super();
+		this.cursos = null;
 		// TODO Auto-generated constructor stub ok
 	}
 
 	public Estudiante(String nombre, String apellidos, String id, String estadoCivil, String _curso) {
 		super(nombre, apellidos, id, estadoCivil);
-		this.curso = _curso; 
+		this.cursos.add(_curso); 
 	}
 	
-	public void imprimir() {
+	/*/ GETT & SETT 
+	public String getCursos() {
 		
-	}
-	
-	// GETT & SETT 
-	public String getCurso() {
-		return curso;
+		
+		return cursos.get();
 	}
 
 	public void setCurso(String curso) {
-		this.curso = curso;
-	}
+		this.cursos = curso;
+	}*/
 
 	// TO STRING 
-	@Override
-	public String toString() {
-		return "Estudiante [curso=" + curso + super.toString() + "]";
+	
+	
+	public void imprimir() {
+		
+		System.out.println("\nNombre: " + super.getNombre() + "\n Apellidos: " + super.getApellidos() + "\n ID: " + super.getId() + "\n Estado Civil: " + super.getEstadoCivil());
+		System.out.println("Cursos: ");
+		for(int i=0; i<cursos.size();i++) {
+			System.out.println(cursos.get(i));
+		}
 	} 
 	
-
+	//agregacion de elemento a arraylist cursos evitando repeticiones
+	public void matriculaNuevoCurso(String curso) {
+		boolean cond=true;
+		for(int i=0; i<cursos.size();i++) {
+			if(cursos.get(i).equals(curso)) {
+				System.out.println("el curso ya existe");
+				cond=false;
+			}
+		}
+		if(cond) {
+			cursos.add(curso);
+		}
+		
+	}
+	//eliminar curso en base a busqueda y comparación en arraylist cursos
+	public void eliminarCurso(String curso){
+		boolean b=false;
+		
+		for(int i=0; i<cursos.size();i++) {
+			if(cursos.get(i).equals(curso)) {
+				cursos.remove(i);
+				b=true;				
+			}
+		}
+		if(b) {
+			System.out.println("eliminación exitosa");
+		}else {
+			System.out.println("No se pudo eliminar, no se encontró el curso");
+		}
+	}
 }
